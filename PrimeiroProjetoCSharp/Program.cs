f2e29a23172c454a0c1668266045dc243d3aed03
adria <adria@DEV-MACHINE>
2025-12-18 05:23:36 +0000

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjetoCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Entre com o seu nome: ");
+String name = Console.ReadLine();
+Console.WriteLine(""
