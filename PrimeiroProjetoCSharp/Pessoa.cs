d691a2195c7cae0ac8b8ecf8e74026c4fe5f0f73
DevSousa <devdiegosousa@gmail.com>
2025-12-18 08:21:21 +0000

    After Hello World!

---
+++ PrimeiroProjetoCSharp/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiroProjetoCSharp
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public byte idade;
+
+
+    }
+}
--- PrimeiroProjetoCSharp/Program.cs
+++ PrimeiroProjetoCSharp/Program.cs
@@ -1,7 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PrimeiroProjetoCSharp;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
 
-Console.WriteLine("Entre com o seu nome: ");
-String name = Console.ReadLine();
-Console.WriteLine(""
+        Console.WriteLine();
+
+        Pessoa pessoa1, pessoa2;
+        pessoa1 = new Pessoa();
+        pessoa2 = new Pessoa();
+
+        Console.WriteLine("Qual o nome da primeira pessoa: ");
+        pessoa1.nome = Console.ReadLine();
+        Console.WriteLine($"Qual a idade de {pessoa1.nome} ");
+        pessoa1.idade = byte.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual o nome da segunda pessoa: ");
+        pessoa2.nome = Console.ReadLine();
+        Console.WriteLine($"Qual a idade de {pessoa2.nome} ");
+        pessoa2.idade = byte.Parse(Console.ReadLine());
+
+        string pessoaMaisVelha = pessoa1.idade > pessoa2.idade ? pessoa1.nome : pessoa2.nome;
+
+        Console.WriteLine("A pessoa mais velha e: " + pessoaMaisVelha);
+
+    }
+}
